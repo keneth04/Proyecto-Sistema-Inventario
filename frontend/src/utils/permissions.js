63f35b1d337d6f301d44d6b399bc6c610539ab11
Keneth Anderson Mejía Lopera <kenethlopera@gmail.com>
2026-04-21 16:08:43 -0500

    Mejora: ultima actualizacion estable del proyecto

--- frontend/src/utils/permissions.js
+++ frontend/src/utils/permissions.js
@@ -3,5 +3,5 @@ import { ROLES } from '../constants/roles';
 export const INVENTORY_MANAGER_ROLES = [ROLES.ADMIN, ROLES.INVENTORY_MANAGER];
 
 export function canManageInventory(user) {
-  return INVENTORY_MANAGER_ROLES.includes(user?.role);
+    return INVENTORY_MANAGER_ROLES.includes(user?.role);
 }
